68dd7e167f0f06a1109b86d5c02d7b9cddc99cf0
Ana Mayordomo <anamayordomo@Anas-Laptop.localdomain>
2024-08-20 11:41:49 -0500

    Handle form submissions with django forms library

--- first_app/forms_django.py
+++ first_app/forms_django.py
@@ -1,6 +1,7 @@
 from django import forms 
 
 class user_form(forms.Form):
-    user_name = forms.CharField()
-    user_email = forms.EmailField()
+    user_name = forms.CharField(label="Full Name", widget=forms.TextInput(attrs={'placeholder':"Enter your full name", 'style': "width:300px"}))
+    user_dob = forms.DateField(label="Date of Birth", widget=forms.TextInput(attrs={'type' : "date"}))
+    user_email = forms.EmailField(label="Email")
 
--- first_app/views.py
+++ first_app/views.py
@@ -12,4 +12,18 @@ def index(request):
 def form(request):
     new_form = forms_django.user_form()
     diction = {'test_form': new_form, 'heading_1': "This is from Django form library"}
+
+    if request.method == "POST":
+        new_form = forms_django.user_form(request.POST)
+
+        if new_form.is_valid():
+            user_name = new_form.cleaned_data['user_name']
+            user_dob = new_form.cleaned_data['user_dob']
+            user_email = new_form.cleaned_data['user_email']
+
+            diction.update({'user_name' : user_name})
+            diction.update({'user_dob' : user_dob})
+            diction.update({'user_email' : user_email})
+            diction.update({'form_submitted' : "Yes"})
+
     return render(request, 'first_app/form.html', context=diction)
