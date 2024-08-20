fbb0da81bef5d423415e1c715bf7beaf121f9078
Ana Mayordomo <anamayordomo@Anas-Laptop.localdomain>
2024-08-20 11:00:51 -0500

    Create simple form using HTML tags

--- first_app/views.py
+++ first_app/views.py
@@ -7,3 +7,7 @@ def index(request):
 
     diction = {'text_1': 'This is a list of musicians from our database', 'musician': musician_list}
     return render(request, 'first_app/index.html', context=diction)
+
+def form(request):
+    diction = {}
+    return render(request, 'first_app/form.html', context=diction)
